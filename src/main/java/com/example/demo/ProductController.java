1a0619a69f58d4c1e56a3f399e139419e7f421ed
JangAyeon <ayeon3052@ewhain.net>
2026-03-01 14:33:28 +0900

    first commit

---
+++ src/main/java/com/example/demo/ProductController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class ProductController {
+   ProductController(){
+       System.out.println("진짜 스프링이 알아서 객체 생성을 하나?");
+   }
+
+   @RequestMapping(value="", method= RequestMethod.GET)
+    public String getProduct(){
+       return "NoteBook";
+   }
+}
